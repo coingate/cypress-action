18d85d8cc5173fe73637ecf979b920ea9b11ceea
EvaldasB <kasredas@gmail.com>
2022-10-14 09:50:16 +0300

    node 14

--- dist/index.js
+++ dist/index.js
@@ -103476,7 +103476,7 @@ var runTests = function (options) {
                     npxPath = _a.sent();
                     if (!useSorryCypress) return [3 /*break*/, 3];
                     date = new Date();
-                    return [4 /*yield*/, exec.exec("npx cy2 run --parallel --record --key merged --ci-build-id \"".concat(date.toLocaleString(), " | ").concat(browserInput, " | ").concat(options.spec.slice(35, options.spec.length), "\""), cmd, opts)];
+                    return [4 /*yield*/, exec.exec("".concat(quote_default()(npxPath), " cy2 run --parallel --record --key merged --ci-build-id \"").concat(date.toLocaleString(), " | ").concat(browserInput, " | ").concat(options.spec.slice(35, options.spec.length), "\""), cmd, opts)];
                 case 2:
                     _a.sent();
                     return [3 /*break*/, 5];
